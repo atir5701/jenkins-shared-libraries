a35d87e8b203e380f4331ccba37a594c82d7925d
atir_5701 <shakhrelia.a@northeastern.edu>
2025-03-07 10:24:25 -0500

    Update gitCloning.groovy

--- vars/gitCloning.groovy
+++ vars/gitCloning.groovy
@@ -1,4 +1,4 @@
-def myfunction(){
+def call(){
   echo "This is cloning the code"
   git url: "https://github.com/LondheShubham153/django-notes-app.git", branch:"main"
   echo "Code cloned successfully"
